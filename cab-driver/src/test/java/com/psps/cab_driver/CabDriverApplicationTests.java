d9a1db557623610c02bb22cad67164467c75adbb
Santhosh PSPS <santhoshpsp4@gmail.com>
2024-06-08 11:05:04 +0530

    Initial commit

---
+++ cab-driver/src/test/java/com/psps/cab_driver/CabDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psps.cab_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ cab-user/src/test/java/com/psps/cab_user/CabUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psps.cab_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
